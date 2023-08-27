f656ddb4a22460a86cc98a2d60efa0da5c76c385
Masaki Koyanagi <mascii@gmail.com>
2023-08-27 16:13:42 +0900

    refactor: use addEventListener instead of window.onload

--- scripts/src/index.ts
+++ scripts/src/index.ts
@@ -1,24 +1,9 @@
-// CVR書き込み
-window.onload = function () {
-  const eventStatsElements = document.getElementsByClassName(
-    'EventStatsHero stats_hero_area flex-row'
-  );
-  const pageview =
-    +document.getElementsByClassName('PageviewsHero num')[0].innerHTML;
-  const visitor =
-    +document.getElementsByClassName('VisitorsHero num')[0].innerHTML;
-  const participation = +document.getElementsByClassName(
-    'ParticipationsHero num'
-  )[0].innerHTML;
-
-  const conversionRatePV = Math.floor((participation / pageview) * 1000) / 10;
-  const conversionRatePVElement = createConversionPVElement(conversionRatePV);
-
-  const conversionRateUU = Math.floor((participation / visitor) * 1000) / 10;
-  const conversionRateUUElement = createConversionUUElement(conversionRateUU);
-
-  eventStatsElements[0].appendChild(conversionRatePVElement);
-  eventStatsElements[0].appendChild(conversionRateUUElement);
+const onReady = (cb: () => void) => {
+  if (document.readyState !== 'loading') {
+    cb();
+  } else {
+    document.addEventListener('DOMContentLoaded', cb);
+  }
 };
 
 const createConversionPVElement = (value: number) => {
@@ -44,3 +29,26 @@ const createConversionUUElement = (value: number) => {
   element.style.color = '#0090c9';
   return element;
 };
+
+// CVR書き込み
+onReady(() => {
+  const eventStatsElements = document.getElementsByClassName(
+    'EventStatsHero stats_hero_area flex-row'
+  );
+  const pageview =
+    +document.getElementsByClassName('PageviewsHero num')[0].innerHTML;
+  const visitor =
+    +document.getElementsByClassName('VisitorsHero num')[0].innerHTML;
+  const participation = +document.getElementsByClassName(
+    'ParticipationsHero num'
+  )[0].innerHTML;
+
+  const conversionRatePV = Math.floor((participation / pageview) * 1000) / 10;
+  const conversionRatePVElement = createConversionPVElement(conversionRatePV);
+
+  const conversionRateUU = Math.floor((participation / visitor) * 1000) / 10;
+  const conversionRateUUElement = createConversionUUElement(conversionRateUU);
+
+  eventStatsElements[0].appendChild(conversionRatePVElement);
+  eventStatsElements[0].appendChild(conversionRateUUElement);
+});
